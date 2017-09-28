a8a06945bb8a4657b6d94a9e82b1ba6baaa86131
J <dev.jjkuo@gmail.com>
2017-09-28 13:53:09 -0500

    added run files

---
+++ logger.py
@@ -0,0 +1,43 @@
+import datetime
+import logging
+import os
+
+from ux2d_project import settings
+
+
+class Log:
+    def __init__(self):
+        log_format = '[%(asctime)s] [%(levelname)s]: %(message)s'
+
+        if not os.path.isdir("logs/"):
+            os.makedirs("logs/")
+
+        logging.basicConfig(
+            filename='logs/%s-project.log' % datetime.date.today(),
+            level=settings.LOG_LEVEL,
+            format=log_format
+        )
+
+    @staticmethod
+    def d(msg):
+        print("[DEBUG]: %s" % msg)
+        logging.debug("%s" % msg)
+
+    @staticmethod
+    def i(msg):
+        print("[INFO]: %s" % msg)
+        logging.info("%s" % msg)
+
+    @staticmethod
+    def w(msg):
+        print("[WARNING]: %s" % msg)
+        logging.warning("%s" % msg)
+
+    @staticmethod
+    def e(msg):
+        print("[ERROR]: %s" % msg)
+        logging.error("%s" % msg)
+
+    @staticmethod
+    def v(msg):
+        print("[VERBOSE]: %s" % msg)
--- teamtrack/apps.py
+++ teamtrack/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
+from logger import Log
+
 
 class TeamtrackConfig(AppConfig):
     name = 'teamtrack'
+
+    def ready(self):
+        Log()
